15c9cb15d2507e1442c39f11fd61a24f0f4bd402
Alexander <cat@caspiantechnologies.org>
2018-06-25 17:59:30 +0700

    Implement a distributed signing scheme.

--- test/test.js
+++ test/test.js
@@ -1,45 +1,52 @@
 ((
     core,
+    assert,
+    eddsa,
+    Buffer,
     nem
 )=>{
+    const Helpers = nem.utils.helpers;
     const Convert = nem.utils.convert;
     const Serialization = nem.utils.serialization;
     const Requests = nem.com.requests;
+    const Network = nem.model.network;
+    const Transactions = nem.model.transactions;
+    const Fees = nem.model.fees;
+    const TransactionTypes = nem.model.transactionTypes;
+    const Objects = nem.model.objects;
+
+    const construct = function(senderPublicKey, recipientCompressedKey, amount, message, msgFee, due, mosaics, mosaicsFee, network) {
+        let timeStamp = Helpers.createNEMTimeStamp();
+        let version = mosaics ? Network.getVersion(2, network) : Network.getVersion(1, network);
+        let data = Objects.create("commonTransactionPart")(TransactionTypes.transfer, senderPublicKey, timeStamp, due, version);
+        let fee = mosaics ? mosaicsFee : Fees.currentFeeFactor * Fees.calculateMinimum(amount / 1000000);
+        let totalFee = Math.floor((msgFee + fee) * 1000000);
+        let custom = {
+            'recipient': recipientCompressedKey.toUpperCase().replace(/-/g, ''),
+            'amount': amount,
+            'fee': totalFee,
+            'message': message,
+            'mosaics': mosaics
+        };
+        return Helpers.extendObj(data, custom);
+    };
+
+    const prepare = function(publicKey, tx, network){
+        assert(!tx.isMultisig);
+
+        let actualSender = publicKey;
+        let recipientCompressedKey = tx.recipient.toString();
+        let amount = Math.round(tx.amount * 1000000);
+        let message = Transactions.prepareMessage(null, tx);
+        let msgFee = Fees.calculateMessage(message, false);
+        let due = network === Network.data.testnet.id ? 60 : 24 * 60;
+        let mosaics = null;
+        let mosaicsFee = null;
+        return construct(actualSender, recipientCompressedKey, amount, message, msgFee, due, mosaics, mosaicsFee, network);
+    };
 
     const endpoint = nem.model.objects.create("endpoint")(nem.model.nodes.defaultTestnet, nem.model.nodes.defaultPort);
 
-    const secret = core.Utils.randomBytes(32);
-
-    const common = nem.model.objects.create("common")('', secret.toString('hex'));
-
-    const transferTransaction = nem.model.objects.create("transferTransaction")("TBCI2A67UQZAKCR6NS4JWAEICEIGEIM72G3MVW5S", 10, "Hello");
-
-    const transactionEntity = nem.model.transactions.prepare("transferTransaction")(common, transferTransaction, nem.model.network.data.testnet.id);
-
-    const hash = Serialization.serializeTransaction(transactionEntity);
-
-    const keyPair = core.KeyPair.fromSecret(secret, 'ed25519');
-
-    const signature = keyPair.sign(hash).toHex().toLowerCase();
-
-    const blob = {
-        'data': Convert.ua2hex(hash),
-        'signature': signature
-    };
-
-    Requests.transaction.announce(endpoint, JSON.stringify(blob)).then(r => {
-        console.log(r);
-    });
-})(
-    require('..'),
-    require('nem-sdk').default
-);
-
-((
-    core,
-    assert,
-    eddsa
-) => {
     const seed = core.Utils.randomBytes(64);
 
     const keyChain = core.KeyChain.fromSeed(seed);
@@ -47,13 +54,8 @@
     const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
     const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
 
-    const messageBytes = core.Utils.randomBytes(32);
-
     let initiator = core.CompoundKeyEddsa.fromSecret(initiatorPrivateBytes, 'ed25519');
     let verifier = core.CompoundKeyEddsa.fromSecret(verifierPrivateBytes, 'ed25519');
-
-    initiator = core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(initiator))));
-    verifier = core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(verifier))));
 
     const iSyncSession = initiator.startSyncSession();
     const vSyncSession = verifier.startSyncSession();
@@ -70,8 +72,16 @@
     initiator.importSyncData(iSyncData);
     verifier.importSyncData(vSyncData);
 
-    const iSigner = initiator.startSignSession(messageBytes);
-    const vSigner = verifier.startSignSession(messageBytes);
+    const transferTransaction = nem.model.objects.create("transferTransaction")("TBCI2A67UQZAKCR6NS4JWAEICEIGEIM72G3MVW5S", 10, "Hello");
+
+    const publicKey = Buffer.from(eddsa.encodePoint(initiator.compoundPublic)).toString('hex');
+
+    const transactionEntity = prepare(publicKey, transferTransaction, nem.model.network.data.testnet.id);
+
+    const hash = Serialization.serializeTransaction(transactionEntity);
+
+    const iSigner = initiator.startSignSession(hash);
+    const vSigner = verifier.startSignSession(hash);
 
     const iSCommitment = iSigner.createCommitment();
     const vSCommitment = vSigner.createCommitment();
@@ -84,13 +94,24 @@
 
     const vPartialSignature = vSigner.computePartialSignature();
 
-    const signature = iSigner.combineSignatures(vPartialSignature);
+    const signature = iSigner.combineSignatures(vPartialSignature).toHex().toLowerCase();
 
-    assert(eddsa.verify(messageBytes, signature, initiator.compoundPublicKey));
+    //const signature = initiator.localPrivateKey.sign(hash).toHex().toLowerCase();
 
-    console.log('Eddsa OK');
+    assert(nem.crypto.verifySignature(publicKey, hash, signature));
+
+    const blob = {
+        'data': Convert.ua2hex(hash),
+        'signature': signature
+    };
+
+    Requests.transaction.announce(endpoint, JSON.stringify(blob)).then(r => {
+        console.log(r);
+    });
 })(
     require('..'),
     require('assert'),
-    require('elliptic').eddsa('ed25519')
+    require('elliptic').eddsa('ed25519'),
+    require('buffer').Buffer,
+    require('nem-sdk').default
 );
