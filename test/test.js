3d7ce02c68993b1d5bf10347ebb382b9923c8284
Alexander <cat@caspiantechnologies.org>
2018-06-27 19:13:18 +0700

    [WIP] Update ECDSA to the new marshalling technique
    SPATIUM-312

--- test/test.js
+++ test/test.js
@@ -1,81 +1,182 @@
+// (async (
+//     core,
+//     assert,
+//     Buffer
+// ) => {
+//     const rewrap = arg => core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(arg))));
+//
+//     try {
+//         const seed = Buffer.from('9ff992e811d4b2d2407ad33b263f567698c37bd6631bc0db90223ef10bce7dca28b8c670522667451430a1cb10d1d6b114234d1c2220b2f4229b00cadfc91c4d', 'hex');
+//
+//         const keyChain = core.KeyChain.fromSeed(seed);
+//
+//         const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
+//         const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
+//
+//         const initiator = rewrap(core.CompoundKeyEddsa.fromOptions({
+//             curve: 'ed25519',
+//             secret: initiatorPrivateBytes
+//         }));
+//
+//         const verifier = rewrap(core.CompoundKeyEddsa.fromOptions({
+//             curve: 'ed25519',
+//             secret: verifierPrivateBytes
+//         }));
+//
+//         const iSyncSession = rewrap(initiator.startSyncSession());
+//         const vSyncSession = rewrap(verifier.startSyncSession());
+//
+//         const iCommitment = rewrap(iSyncSession.createCommitment());
+//         const vCommitment = rewrap(vSyncSession.createCommitment());
+//
+//         const iDecommitment = rewrap(iSyncSession.processCommitment(vCommitment));
+//         const vDecommitment = rewrap(vSyncSession.processCommitment(iCommitment));
+//
+//         const iSyncData = rewrap(iSyncSession.processDecommitment(vDecommitment));
+//         const vSyncData = rewrap(vSyncSession.processDecommitment(iDecommitment));
+//
+//         initiator.importSyncData(iSyncData);
+//         verifier.importSyncData(vSyncData);
+//
+//         const nemWallet = core.NemWallet.fromOptions({
+//             network: core.NemWallet.Testnet,
+//             point: initiator.compoundPublic
+//         });
+//
+//         console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
+//
+//         const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
+//         const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
+//
+//         iTX.startSignSession(initiator);
+//         vTX.startSignSession(verifier);
+//
+//         const iSCommitment = rewrap(iTX.createCommitment());
+//         const vSCommitment = rewrap(vTX.createCommitment());
+//
+//         const iSDecommitment = rewrap(iTX.processCommitment(vSCommitment));
+//         const vSDecommitment = rewrap(vTX.processCommitment(iSCommitment));
+//
+//         iTX.processDecommitment(vSDecommitment);
+//         vTX.processDecommitment(iSDecommitment);
+//
+//         const vPartialSignature = rewrap(vTX.computeSignature());
+//
+//         iTX.applySignature(vPartialSignature);
+//
+//         assert(iTX.verify());
+//
+//         const blob = iTX.toRaw();
+//
+//         await nemWallet.sendSignedTransaction(blob);
+//
+//         console.log('OK');
+//     } catch (e) {
+//         console.error(e);
+//     }
+// })(
+//     require('..'),
+//     require('assert'),
+//     require('buffer').Buffer
+// );
+
 (async (
-    core,
-    assert,
-    Buffer
+  core,
+  assert,
+  Buffer
 ) => {
-    const rewrap = arg => core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(arg))));
+  const rewrap = arg => core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(arg))));
 
-    try {
-        const seed = Buffer.from('9ff992e811d4b2d2407ad33b263f567698c37bd6631bc0db90223ef10bce7dca28b8c670522667451430a1cb10d1d6b114234d1c2220b2f4229b00cadfc91c4d', 'hex');
+  try {
+    const seed = Buffer.from('9ff992e811d4b2d2407ad33b263f567698c37bd6631bc0db90223ef10bce7dca28b8c670522667451430a1cb10d1d6b114234d1c2220b2f4229b00cadfc91c4d', 'hex');
 
-        const keyChain = core.KeyChain.fromSeed(seed);
+    const keyChain = core.KeyChain.fromSeed(seed);
 
-        const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
-        const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
+    const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
+    const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
 
-        const initiator = rewrap(core.CompoundKeyEddsa.fromOptions({
-            curve: 'ed25519',
-            secret: initiatorPrivateBytes
-        }));
+    const paillierKeys = core.CompoundKeyEcdsa.generatePaillierKeys();
 
-        const verifier = rewrap(core.CompoundKeyEddsa.fromOptions({
-            curve: 'ed25519',
-            secret: verifierPrivateBytes
-        }));
+    const initiator = rewrap(core.CompoundKeyEcdsa.fromOptions({
+      curve: 'secp256k1',
+      secret: initiatorPrivateBytes,
+      paillierKeys
+    }));
 
-        const iSyncSession = rewrap(initiator.startSyncSession());
-        const vSyncSession = rewrap(verifier.startSyncSession());
+    const verifier = rewrap(core.CompoundKeyEcdsa.fromOptions({
+      curve: 'secp256k1',
+      secret: verifierPrivateBytes,
+      paillierKeys
+    }));
 
-        const iCommitment = rewrap(iSyncSession.createCommitment());
-        const vCommitment = rewrap(vSyncSession.createCommitment());
+    const iProover = rewrap(initiator.startSyncSession());
+    const vProover = rewrap(verifier.startSyncSession());
 
-        const iDecommitment = rewrap(iSyncSession.processCommitment(vCommitment));
-        const vDecommitment = rewrap(vSyncSession.processCommitment(iCommitment));
+    const iiCommitment = rewrap(iProover.createInitialCommitment());
+    const viCommitment = rewrap(vProover.createInitialCommitment());
 
-        const iSyncData = rewrap(iSyncSession.processDecommitment(vDecommitment));
-        const vSyncData = rewrap(vSyncSession.processDecommitment(iDecommitment));
+    const iiDecommitment = rewrap(iProover.processInitialCommitment(viCommitment));
+    const viDecommitment = rewrap(vProover.processInitialCommitment(iiCommitment));
 
-        initiator.importSyncData(iSyncData);
-        verifier.importSyncData(vSyncData);
+    const iVerifier = rewrap(iProover.processInitialDecommitment(viDecommitment));
+    const vVerifier = rewrap(vProover.processInitialDecommitment(iiDecommitment));
 
-        const nemWallet = core.NemWallet.fromOptions({
-            network: core.NemWallet.Testnet,
-            point: initiator.compoundPublic
-        });
+    const ivCommitment = rewrap(iVerifier.createCommitment());
+    const vvCommitment = rewrap(vVerifier.createCommitment());
 
-        console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
+    const ipCommitment = rewrap(iProover.processCommitment(vvCommitment));
+    const vpCommitment = rewrap(vProover.processCommitment(ivCommitment));
 
-        const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
-        const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
+    const ivDecommitment = rewrap(iVerifier.processCommitment(vpCommitment));
+    const vvDecommitment = rewrap(vVerifier.processCommitment(ipCommitment));
 
-        iTX.startSignSession(initiator);
-        vTX.startSignSession(verifier);
+    const ipDecommitment = rewrap(iProover.processDecommitment(vvDecommitment));
+    const vpDecommitment = rewrap(vProover.processDecommitment(ivDecommitment));
 
-        const iSCommitment = rewrap(iTX.createCommitment());
-        const vSCommitment = rewrap(vTX.createCommitment());
+    const iSyncData = rewrap(iVerifier.processDecommitment(vpDecommitment));
+    const vSyncData = rewrap(vVerifier.processDecommitment(ipDecommitment));
 
-        const iSDecommitment = rewrap(iTX.processCommitment(vSCommitment));
-        const vSDecommitment = rewrap(vTX.processCommitment(iSCommitment));
+    initiator.importSyncData(iSyncData);
+    verifier.importSyncData(vSyncData);
 
-        iTX.processDecommitment(vSDecommitment);
-        vTX.processDecommitment(iSDecommitment);
+    const nemWallet = core.EthereumWallet.fromOptions({
+      network: core.NemWallet.Testnet,
+      point: initiator.compoundPublic
+    });
 
-        const vPartialSignature = rewrap(vTX.computeSignature());
+    console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
 
-        iTX.applySignature(vPartialSignature);
+    const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
+    const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
 
-        assert(iTX.verify());
+    iTX.startSignSession(initiator);
+    vTX.startSignSession(verifier);
 
-        const blob = iTX.toRaw();
+    const iSCommitment = rewrap(iTX.createCommitment());
+    const vSCommitment = rewrap(vTX.createCommitment());
 
-        await nemWallet.sendSignedTransaction(blob);
+    const iSDecommitment = rewrap(iTX.processCommitment(vSCommitment));
+    const vSDecommitment = rewrap(vTX.processCommitment(iSCommitment));
 
-        console.log('OK');
-    } catch (e) {
-        console.error(e);
-    }
+    iTX.processDecommitment(vSDecommitment);
+    vTX.processDecommitment(iSDecommitment);
+
+    const vPartialSignature = rewrap(vTX.computeSignature());
+
+    iTX.applySignature(vPartialSignature);
+
+    assert(iTX.verify());
+
+    const blob = iTX.toRaw();
+
+    await nemWallet.sendSignedTransaction(blob);
+
+    console.log('OK');
+  } catch (e) {
+    console.error(e);
+  }
 })(
-    require('..'),
-    require('assert'),
-    require('buffer').Buffer
+  require('..'),
+  require('assert'),
+  require('buffer').Buffer
 );
