df273b7941491a37d93b6c87c67df66fdee97ccc
Alexander <cat@caspiantechnologies.org>
2018-06-28 12:10:36 +0700

    [WIP] Update ECDSA prmitives and Ethereum transaction
    SPATIUM-312

--- test/test.js
+++ test/test.js
@@ -1,84 +1,84 @@
-// (async (
-//     core,
-//     assert,
-//     Buffer
-// ) => {
-//     const rewrap = arg => core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(arg))));
-//
-//     try {
-//         const seed = Buffer.from('9ff992e811d4b2d2407ad33b263f567698c37bd6631bc0db90223ef10bce7dca28b8c670522667451430a1cb10d1d6b114234d1c2220b2f4229b00cadfc91c4d', 'hex');
-//
-//         const keyChain = core.KeyChain.fromSeed(seed);
-//
-//         const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
-//         const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
-//
-//         const initiator = rewrap(core.CompoundKeyEddsa.fromOptions({
-//             curve: 'ed25519',
-//             secret: initiatorPrivateBytes
-//         }));
-//
-//         const verifier = rewrap(core.CompoundKeyEddsa.fromOptions({
-//             curve: 'ed25519',
-//             secret: verifierPrivateBytes
-//         }));
-//
-//         const iSyncSession = rewrap(initiator.startSyncSession());
-//         const vSyncSession = rewrap(verifier.startSyncSession());
-//
-//         const iCommitment = rewrap(iSyncSession.createCommitment());
-//         const vCommitment = rewrap(vSyncSession.createCommitment());
-//
-//         const iDecommitment = rewrap(iSyncSession.processCommitment(vCommitment));
-//         const vDecommitment = rewrap(vSyncSession.processCommitment(iCommitment));
-//
-//         const iSyncData = rewrap(iSyncSession.processDecommitment(vDecommitment));
-//         const vSyncData = rewrap(vSyncSession.processDecommitment(iDecommitment));
-//
-//         initiator.importSyncData(iSyncData);
-//         verifier.importSyncData(vSyncData);
-//
-//         const nemWallet = core.NemWallet.fromOptions({
-//             network: core.NemWallet.Testnet,
-//             point: initiator.compoundPublic
-//         });
-//
-//         console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
-//
-//         const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
-//         const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
-//
-//         iTX.startSignSession(initiator);
-//         vTX.startSignSession(verifier);
-//
-//         const iSCommitment = rewrap(iTX.createCommitment());
-//         const vSCommitment = rewrap(vTX.createCommitment());
-//
-//         const iSDecommitment = rewrap(iTX.processCommitment(vSCommitment));
-//         const vSDecommitment = rewrap(vTX.processCommitment(iSCommitment));
-//
-//         iTX.processDecommitment(vSDecommitment);
-//         vTX.processDecommitment(iSDecommitment);
-//
-//         const vPartialSignature = rewrap(vTX.computeSignature());
-//
-//         iTX.applySignature(vPartialSignature);
-//
-//         assert(iTX.verify());
-//
-//         const blob = iTX.toRaw();
-//
-//         await nemWallet.sendSignedTransaction(blob);
-//
-//         console.log('OK');
-//     } catch (e) {
-//         console.error(e);
-//     }
-// })(
-//     require('..'),
-//     require('assert'),
-//     require('buffer').Buffer
-// );
+(async (
+    core,
+    assert,
+    Buffer
+) => {
+    const rewrap = arg => core.Marshal.unwrap(JSON.parse(JSON.stringify(core.Marshal.wrap(arg))));
+
+    try {
+        const seed = Buffer.from('9ff992e811d4b2d2407ad33b263f567698c37bd6631bc0db90223ef10bce7dca28b8c670522667451430a1cb10d1d6b114234d1c2220b2f4229b00cadfc91c4d', 'hex');
+
+        const keyChain = core.KeyChain.fromSeed(seed);
+
+        const initiatorPrivateBytes = keyChain.getAccountSecret(60, 0);
+        const verifierPrivateBytes = keyChain.getAccountSecret(60, 1);
+
+        const initiator = rewrap(core.CompoundKeyEddsa.fromOptions({
+            curve: 'ed25519',
+            secret: initiatorPrivateBytes
+        }));
+
+        const verifier = rewrap(core.CompoundKeyEddsa.fromOptions({
+            curve: 'ed25519',
+            secret: verifierPrivateBytes
+        }));
+
+        const iSyncSession = rewrap(initiator.startSyncSession());
+        const vSyncSession = rewrap(verifier.startSyncSession());
+
+        const iCommitment = rewrap(iSyncSession.createCommitment());
+        const vCommitment = rewrap(vSyncSession.createCommitment());
+
+        const iDecommitment = rewrap(iSyncSession.processCommitment(vCommitment));
+        const vDecommitment = rewrap(vSyncSession.processCommitment(iCommitment));
+
+        const iSyncData = rewrap(iSyncSession.processDecommitment(vDecommitment));
+        const vSyncData = rewrap(vSyncSession.processDecommitment(iDecommitment));
+
+        initiator.importSyncData(iSyncData);
+        verifier.importSyncData(vSyncData);
+
+        const nemWallet = core.NemWallet.fromOptions({
+            network: core.NemWallet.Testnet,
+            point: initiator.compoundPublic
+        });
+
+        console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
+
+        const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', nemWallet.toInternal(1)));
+        const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', nemWallet.toInternal(1)));
+
+        iTX.startSignSession(initiator);
+        vTX.startSignSession(verifier);
+
+        const iSCommitment = rewrap(iTX.createCommitment());
+        const vSCommitment = rewrap(vTX.createCommitment());
+
+        const iSDecommitment = rewrap(iTX.processCommitment(vSCommitment));
+        const vSDecommitment = rewrap(vTX.processCommitment(iSCommitment));
+
+        iTX.processDecommitment(vSDecommitment);
+        vTX.processDecommitment(iSDecommitment);
+
+        const vPartialSignature = rewrap(vTX.computeSignature());
+
+        iTX.applySignature(vPartialSignature);
+
+        assert(iTX.verify());
+
+        const blob = iTX.toRaw();
+
+        await nemWallet.sendSignedTransaction(blob);
+
+        console.log('OK');
+    } catch (e) {
+        console.error(e);
+    }
+})(
+    require('..'),
+    require('assert'),
+    require('buffer').Buffer
+);
 
 (async (
   core,
@@ -139,15 +139,16 @@
     initiator.importSyncData(iSyncData);
     verifier.importSyncData(vSyncData);
 
-    const nemWallet = core.EthereumWallet.fromOptions({
-      network: core.NemWallet.Testnet,
-      point: initiator.compoundPublic
+    const ethWallet = core.EthereumWallet.fromOptions({
+      network: core.EthereumWallet.Testnet,
+      point: initiator.compoundPublic,
+      endpoint: 'https://rinkeby.infura.io/dlYX0gLUjGGCk7IBFq2C'
     });
 
-    console.log(nemWallet.address, ':', nemWallet.fromInternal((await nemWallet.getBalance()).unconfirmed), 'NEM');
+    console.log(ethWallet.address, ':', ethWallet.fromInternal((await ethWallet.getBalance()).unconfirmed), 'ETH');
 
-    const iTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
-    const vTX = rewrap(await nemWallet.prepareTransaction(rewrap(core.NemTransaction.create()), 'TCLT5GRRTWIOHXE2NGXLAXLTU24OSM7YZXBDBEZR', 10));
+    let iTX = rewrap(await ethWallet.prepareTransaction(rewrap(core.EthereumTransaction.create()), '0x28ea64196afb9b0a15dadad1cf8fcfb5669973d5', ethWallet.toInternal(0.01)));
+    let vTX = rewrap(await ethWallet.prepareTransaction(rewrap(core.EthereumTransaction.create()), '0x28ea64196afb9b0a15dadad1cf8fcfb5669973d5', ethWallet.toInternal(0.01)));
 
     iTX.startSignSession(initiator);
     vTX.startSignSession(verifier);
@@ -165,11 +166,13 @@
 
     iTX.applySignature(vPartialSignature);
 
+    iTX = rewrap(iTX);
+
     assert(iTX.verify());
 
     const blob = iTX.toRaw();
 
-    await nemWallet.sendSignedTransaction(blob);
+    await ethWallet.sendSignedTransaction(blob);
 
     console.log('OK');
   } catch (e) {
